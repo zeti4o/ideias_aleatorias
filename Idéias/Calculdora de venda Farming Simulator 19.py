3f8c8d975763815b6475707dee659a684a05e48b
Gabriel Franco <fgabrielc.05@gmail.com>
2021-12-03 22:52:44 -0300

    Create Calculdora de venda Farming Simulator 19.py

---
+++ Idéias/Calculdora de venda Farming Simulator 19.py
@@ -0,0 +1,6 @@
+print('=' * 30)
+n1 = int(input('\n Litragem do granel: '))
+n2 = int(input(' Preço por mil litros: '))
+n3 = int(n1 * n2 / 1000)
+print(f'\n a o valor total é: {n3}\n')
+print('=' * 30)
